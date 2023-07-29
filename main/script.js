391ca3e04c4217dfda7604d11a48e5f85094f06d
octagony <moctagony@gmail.com>
2023-07-29 12:01:32 +0300

    [FEAT] Update manifest version in manifest.json

--- main/script.js
+++ main/script.js
@@ -1,28 +1,27 @@
 const btn = document.getElementById('mirror-btn')
-const select = document.getElementById('mirrors-list');
+const select = document.getElementById('mirrors-list')
 
-let selectedMirror = (mirror)=>{
-    switch(mirror){
-      case 'garuda':
-        return 'piped.garudalinux.org';
-      case 'rocks':
-        return 'piped.kavin.rocks';
-    }
-};
+const selectedMirror = mirror => {
+	switch (mirror) {
+		case 'garuda':
+			return 'piped.garudalinux.org'
+		case 'rocks':
+			return 'piped.kavin.rocks'
+	}
+}
 
-
-btn.addEventListener('click',()=>{
-   browser.tabs.query({active: true, currentWindow: true}).then((tabs)=>{
-    const replacedUrl = selectedMirror(select.value);
-    let tab = tabs[0].url.replace('www.youtube.com', replacedUrl);
-    if(tab.includes('undefined')){
-      browser.tabs.create({
-        url:`https://piped.garudalinux.org/`
-      });
-     }else{
-      browser.tabs.create({
-        url:`${tab}`
-      });
-     }
-  })
+btn.addEventListener('click', () => {
+	browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
+		const replacedUrl = selectedMirror(select.value)
+		const tab = tabs[0].url.replace('www.youtube.com', replacedUrl)
+		if (tab.includes('undefined')) {
+			browser.tabs.create({
+				url: `https://piped.garudalinux.org/`,
+			})
+		} else {
+			browser.tabs.create({
+				url: `${tab}`,
+			})
+		}
+	})
 })
